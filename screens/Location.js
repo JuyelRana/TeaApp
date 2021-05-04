7e65a460fc38df4b967a739b773f87bd96161e0d
Juyel Rana <juyel.desktopit@gmail.com>
2021-05-05 02:42:37 +0600

    Location Screen Completed

--- screens/Location.js
+++ screens/Location.js
@@ -1,21 +1,302 @@
-import React from 'react';
-import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet
-} from 'react-native';
+import React, {useState} from 'react';
+import {Image, SafeAreaView, StyleSheet, Text, TextInput, View, FlatList, TouchableOpacity} from 'react-native';
+import {toggleTheme} from "../stores/actions/themeActions";
+import {connect} from "react-redux";
+import {COLORS, dummyData, FONTS, icons, SIZES} from "../constants";
+import {IconButton, TabButton} from "../components";
 
-const Location = ({ navigation }) => {
+
+const Location = ({navigation, appTheme}) => {
+
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    const renderHeader = () => {
+        return (
+            <SafeAreaView style={{
+                height: 120,
+                backgroundColor: COLORS.purple,
+                alignItems: "center"
+            }}>
+
+                <View style={{
+                    flexDirection: "row",
+                    paddingHorizontal: SIZES.radius,
+                    alignItems: "center"
+                }}>
+
+                    {/*Back Button*/}
+                    <IconButton
+                        icon={icons.leftArrow}
+                        onPress={() => navigation.goBack()}/>
+
+                    {/*Title */}
+                    <View style={{
+                        flex: 1,
+                        alignItems: "center"
+                    }}>
+                        <Text style={{
+                            color: COLORS.white,
+                            ...FONTS.h1,
+                            fontSize: 25
+                        }}>Locations</Text>
+
+                    </View>
+
+                    {/*Empty */}
+                    <View style={{width: 25}}/>
+
+                </View>
+
+            </SafeAreaView>
+        )
+    }
+
+
+    const renderTopBarSection = () => {
+        return (
+            <View style={{
+                flexDirection: "row",
+                textAlign: 'center',
+                justifyContent: 'center',
+                marginTop: 5
+            }}>
+                {/*NearBy */}
+                <TabButton
+                    containerStyle={{
+                        width: 80
+                    }}
+                    selected={(selectedTab === 0)}
+                    onPress={() => setSelectedTab(0)}
+                    label="Near By"/>
+
+                {/*Previous */}
+                <TabButton
+                    containerStyle={{
+                        width: 100
+                    }}
+                    selected={(selectedTab === 1)}
+                    onPress={() => setSelectedTab(1)}
+                    label="Previous"/>
+
+                {/*Favorite */}
+                <TabButton
+                    containerStyle={{
+                        width: 100
+                    }}
+                    selected={(selectedTab === 2)}
+                    onPress={() => setSelectedTab(2)}
+                    label="Favorite"/>
+
+            </View>
+        )
+    }
+
+
+    const renderSearchBar = () => {
+        return (
+            <View style={{
+                flexDirection: 'row',
+                marginTop: SIZES.radius,
+                marginRight: SIZES.radius,
+                marginLeft: SIZES.radius,
+                height: 50,
+                paddingHorizontal: SIZES.padding,
+                borderRadius: 25,
+                backgroundColor: COLORS.lightGreen2,
+                alignItems: 'center'
+            }}>
+
+                <TextInput
+                    style={{
+                        flex: 1,
+                        height: 50,
+                        color: COLORS.black,
+                        ...FONTS.body3
+                    }}
+                    placeholder="Enter your city, state, or zip code"
+                    placeholderTextColor={COLORS.lightGray2}/>
+
+                <Image
+                    source={icons.search}
+                    style={{
+                        width: 30,
+                        height: 30,
+                        tintColor: COLORS.lightGray2
+                    }}
+                />
+
+            </View>
+        )
+    }
+
+    const renderLocationList = () => {
+        return (
+            <FlatList
+                style={{
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius
+                }}
+                data={dummyData.locations}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                keyboardDismissMode="on-drag"
+                renderItem={({item}) => (
+
+                    <TouchableOpacity
+                        style={{
+                            marginBottom: SIZES.radius,
+                            borderRadius: SIZES.radius * 2,
+                            paddingHorizontal: SIZES.padding,
+                            paddingVertical: SIZES.radius,
+                            backgroundColor: appTheme.cardBackgroundColor
+                        }}
+                        onPress={() => navigation.navigate("Order", {
+                            selectedLocation: item
+                        })}>
+
+                        {/*Name & Bookmark*/}
+                        <View
+                            style={{
+                                flexDirection: 'row'
+                            }}>
+                            <Text
+                                style={{
+                                    flex: 1,
+                                    color: appTheme.textColor,
+                                    ...FONTS.h2
+                                }}>
+                                {item.title}
+                            </Text>
+
+                            <Image
+                                source={item.bookmarked ? icons.bookmarkFilled : icons.bookmark}
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: item.bookmarked ? COLORS.red : COLORS.white
+                                }}
+                            />
+                        </View>
+
+                        {/*Address*/}
+                        <View
+                            style={{
+                                marginTop: SIZES.base,
+                                width: "80%"
+                            }}>
+
+                            <Text
+                                style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body3,
+                                    lineHeight: 21
+                                }}
+                            >
+                                {item.address}
+                            </Text>
+
+                        </View>
+
+                        {/*Operation Hours*/}
+                        <View
+                            style={{
+                                marginTop: SIZES.base
+                            }}>
+
+                            <Text
+                                style={{
+                                    color: appTheme.textColor,
+                                    ...FONTS.body5,
+                                    lineHeight: 16
+                                }}>
+                                {item.operation_hours}
+                            </Text>
+
+                        </View>
+
+                        {/*Services*/}
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: SIZES.base
+                            }}>
+
+                            {/*Pick Up*/}
+                            <View
+                                style={{
+                                    borderColor: appTheme.textColor,
+                                    borderWidth: 1,
+                                    borderRadius: 20,
+                                    paddingHorizontal: SIZES.radius,
+                                    paddingVertical: 5
+                                }}>
+
+                                <Text
+                                    style={{
+                                        color: appTheme.textColor,
+                                        ...FONTS.body3
+                                    }}
+                                >
+                                    Pick Up
+                                </Text>
+
+                            </View>
+
+                            {/*Delivery*/}
+
+                            <View
+                                style={{
+                                    borderColor: appTheme.textColor,
+                                    borderWidth: 1,
+                                    borderRadius: 20,
+                                    paddingHorizontal: SIZES.radius,
+                                    paddingVertical: 5,
+                                    marginLeft: 5
+                                }}>
+
+                                <Text
+                                    style={{
+                                        color: appTheme.textColor,
+                                        ...FONTS.body3
+                                    }}
+                                >
+                                    Delivery
+                                </Text>
+
+                            </View>
+
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
+        )
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Location</Text>
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Order")}
-            >
-                <Text>Navigate to Order</Text>
-            </TouchableOpacity>
+            {/*Header */}
+            {renderHeader()}
+
+            {/*Details */}
+            <View style={{
+                flex: 1,
+                backgroundColor: appTheme.backgroundColor,
+                marginTop: -20,
+                borderTopLeftRadius: SIZES.radius * 2,
+                borderTopRightRadius: SIZES.radius * 2
+            }}>
+
+                {/*Top Bar Tab Section*/}
+                {renderTopBarSection()}
+
+                {/*Search Bar Section*/}
+                {renderSearchBar()}
+
+                {/*List of Location*/}
+                {renderLocationList()}
+
+            </View>
         </View>
     )
 }
@@ -23,9 +304,22 @@ const Location = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
     }
 })
 
-export default Location;
+const mapStateToProps = (store) => {
+
+    const {appTheme, error} = store.themeStore;
+    return {appTheme, error};
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleTheme: (themeType) => {
+            return dispatch(toggleTheme(themeType))
+        }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Location);
